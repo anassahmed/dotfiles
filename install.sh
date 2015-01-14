5a53a234de80e0018f3e4487896bdb45184607b2
Anass Ahmed <anass.1430@gmail.com>
2015-01-14 05:17:36 +0200

    Add Initialization Script

---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+echo "Welcome to Anass dotFiles Initialization"
+
+# Git Repo of our Dot Files
+GIT_REPO="git@github.com:anassahmed/dotfiles.git"
+
+# Home Git command to download to the home directory
+HOMEGIT=`git --work-tree=~/ --git-dir=~/.dotfiles.git`
+
+GIT_V=`git --version`
+if [ $? -ne 0 ]
+then
+    echo "Git should be installed before proceeding. please install it and try again"
+    exit
+fi
+
+# Initialize Git repo in the home directory
+$HOMEGIT init
+
+# Add our repo
+$HOMEGIT remote add origin $GIT_REPO
+
+# Pull the contents
+$HOMEGIT pull -u origin master
+
+# Pull the submodules
+$HOMEGIT submodules update --init --recursive
+
+# Install VIM Plugins
+vim +PluginInstall +qall
+
+# Change shell ot ZSH
+chsh -s /bin/zsh
+
+echo "Installation completed successfully."
